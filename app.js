4f7a1eb830460c95b3da5d29d55933500ac9bb8e
Ian Benedict <ianbenedict@gmail.com>
2015-03-30 16:06:22 -0400

    Created test route.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var hapi = require('hapi');
 
-var server = new hapi.Server(+process.env.PORT, '0.0.0.0');
+var server = new hapi.Server();
+
+server.connection({
+  host: '0.0.0.0',
+  port: +process.env.PORT
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello.');
+  }
+});
+ 
+server.start(); 
