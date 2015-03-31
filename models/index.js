5af55b90ca5a6f39f97a49fdcc585086a170f25e
Ian Benedict <ianbenedict@gmail.com>
2015-03-31 09:24:44 -0400

    Added various models.

--- models/index.js
+++ models/index.js
@@ -6,15 +6,24 @@ var sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
   dialectOptions: {
     ssl: true
+  },
+  define: {
+    freezeTableName: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
   }
+});
+
+
+/* Models */
+
+var User = sequelize.define('User', {
+  name: Sequelize.STRING
 });
 
 var Campaign = sequelize.define('campaign', {
   name: Sequelize.STRING,
   description: Sequelize.STRING
-}, {
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
 });
 
 var Character = sequelize.define('character', {
@@ -22,7 +31,37 @@ var Character = sequelize.define('character', {
   bio: Sequelize.STRING
 });
 
+var Item = sequelize.define('item', {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING
+});
+
+var Map_ = sequelize.define('map', {
+  name: Sequelize.STRING
+});
+
+var Blog = sequelize.define('blog', {
+  name: Sequelize.STRING
+});
+
+var BlogPost = sequelize.define('blog_post', {
+  name: Sequelize.STRING,
+  body: Sequelize.STRING
+});
+
+
+/* Relations */
+
+Map_.belongsTo(Campaign);
 Character.belongsTo(Campaign);
+Character.belongsTo(User);
+Item.belongsTo(Character);
+Blog.belongsTo(Campaign);
+BlogPost.belongsTo(Blog);
+BlogPost.belongsTo(User);
+
+
+/* Sync */
 
 Campaign.sync({ force: true }).then(function () {
   return Campaign.create({
