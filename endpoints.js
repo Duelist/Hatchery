2f605dc33119fe6a3909e9d91d10aebcb8abae4b
Ian Benedict <ianbenedict@gmail.com>
2015-04-07 21:03:44 -0400

    Refactored most endpoints.

--- endpoints.js
+++ endpoints.js
@@ -4,7 +4,7 @@ var async = require('async'),
     models = require('./models');
 
 exports.home = function (request, response) {
-  response.view('index', {
+  return response.view('index', {
     member: request.auth.credentials
   });
 }
@@ -19,7 +19,6 @@ exports.login = function (request, response) {
   if (request.method === 'post') {
     if (!request.payload.username || !request.payload.password) {
       message = 'Missing username or password.';
-      response.view('login', { message: message });
     } else {
       models.member.findOne({
         where: {
@@ -33,20 +32,18 @@ exports.login = function (request, response) {
               return response.redirect('/');
             } else {
               message = 'Invalid username or password.';
-              response.view('login', { message: message });
             }
           })
         } else {
           message = 'Invalid username or password.';
-          response.view('login', { message: message });
         }
       });
     }
   }
 
-  if (request.method === 'get') {
-    response.view('login', { message: message });
-  }
+  return response.view('login', {
+    message: message
+  });
 }
 
 exports.logout = function (request, response) {
@@ -55,6 +52,8 @@ exports.logout = function (request, response) {
 }
 
 exports.campaign = function (request, response) {
+  var message = '';
+
   if (request.method === 'post') {
     if (request.payload.name) {
       // Create campaign
@@ -65,115 +64,94 @@ exports.campaign = function (request, response) {
         member_id: request.auth.credentials.id
       }).then(function (campaign) {
         if (campaign) {
-          response.redirect('/');
-        } else {
-          response.view('campaign', {
-            member: request.auth.credentials,
-            message: 'Could not create a new campaign.'
-          });
+          return response.redirect('/');
         }
+
+        message = 'Could not create a new campaign.';
       });
     }
-  } else {
-    response.view('campaign', {
-      member: request.auth.credentials,
-      form_action_url: '/campaign'
-    });
   }
+
+  return response.view('campaign', {
+    member: request.auth.credentials,
+    form_action_url: '/campaign',
+    message: message
+  });
 }
 
 exports.character = function (request, response) {
-  if (request.method === 'post') {
-    if (request.payload.name) {
-      // Create character
-      models.campaign.findOne({
-        where: {
-          slug: request.params.campaign_slug
-        }
-      }).then(function (campaign) {
-        if (campaign) {
-          models.character.create({
-            name: request.payload.name,
-            bio: request.payload.bio,
-            slug: slug(request.payload.name).toLowerCase(),
-            member_id: request.auth.credentials.id,
-            campaign_id: request.params.campaign_id
-          }).then(function (character) {
-            if (character) {
-              response.redirect('/');
-            } else {
-              response.view('character', {
-                member: request.auth.credentials,
-                message: 'Could not create a new character.'
-              });
-            }
-          });
-        } else {
-          response.redirect('/');
-        }
-      });
+  var message = '';
+
+  models.campaign.findOne({
+    where: {
+      slug: request.params.campaign_slug
     }
-  } else {
-    models.campaign.findOne({
-      where: {
-        slug: request.params.campaign_slug
-      }
-    }).then(function (campaign) {
-      if (campaign) {
-        response.view('character', {
-          member: request.auth.credentials,
-          form_action_url: '/campaign/' + request.params.campaign_id + '/character'
+  }).then(function (campaign) {
+    if (!campaign) {
+      return response.redirect('/');
+    }
+
+    if (request.method === 'post') {
+      if (request.payload.name) {
+        models.character.create({
+          name: request.payload.name,
+          bio: request.payload.bio,
+          slug: slug(request.payload.name).toLowerCase(),
+          member_id: request.auth.credentials.id,
+          campaign_id: request.params.campaign_id
+        }).then(function (character) {
+          if (character) {
+            return response.redirect('/');
+          }
+
+          message = 'Could not create a new character.';
         });
-      } else {
-        response.redirect('/');
       }
-    });
-  }
+
+      message = 'Please enter a character name.';
+    }
+  });
+
+  return response.view('character', {
+    member: request.auth.credentials,
+    message: message
+  });
 }
 
 exports.item = function (request, response) {
-  if (request.method === 'post') {
-    if (request.payload.name) {
-      // Create item
-      models.campaign.findOne({
-        where: {
-          slug: request.params.campaign_slug
-        }
-      }).then(function (campaign) {
-        if (campaign) {
-          models.item.create({
-            name: request.payload.name,
-            description: request.payload.description,
-            slug: slug(request.payload.name)
-          }).then(function (item) {
-            if (item) {
-              response.redirect('/');
-            } else {
-              response.view('item', {
-                member: request.auth.credentials,
-                message: 'Could not create a new item.'
-              });
-            }
-          });
-        } else {
-          response.redirect('/');
-        }
-      });
+  var message = '';
+
+  models.campaign.findOne({
+    where: {
+      slug: request.params.campaign_slug
     }
-  } else {
-    models.campaign.findOne({
-      where: {
-        slug: request.params.campaign_slug
-      }
-    }).then(function (campaign) {
-      if (campaign) {
-        response.view('item', {
-          member: request.auth.credentials,
-          form_action_url: '/campaign/' + campaign.slug + '/item'
+  }).then(function (campaign) {
+    if (!campaign) {
+      return response.redirect('/');
+    }
+
+    if (request.method === 'post') {
+      if (request.payload.name) {
+        models.item.create({
+          name: request.payload.name,
+          description: request.payload.bio,
+          slug: slug(request.payload.name).toLowerCase(),
+          campaign_id: request.params.campaign_id
+        }).then(function (item) {
+          if (item) {
+            return response.redirect('/');
+          }
+
+          message = 'Could not create a new item.';
         });
-      } else {
-        response.redirect('/');
       }
-    });
-  }
+
+      message = 'Please enter an item name.';
+    }
+  });
+
+  return response.view('item', {
+    member: request.auth.credentials,
+    message: message
+  });
 }
