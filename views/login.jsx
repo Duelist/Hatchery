ec8ad0fabd9cfcdf48df8d5ecb26a2889d2180be
Ian Benedict <ianbenedict@gmail.com>
2015-04-16 09:32:02 -0400

    Switched login template to JSX.

---
+++ views/login.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var Login = React.createClass({
+  render: function() {
+    return
+      <form action='/login' method='post'>
+        <input name='username' type='text' value=''></input>
+        <input name='password' type='password' value=''></input>
+        <input type='submit' value='Login'></input>
+      </form>
+    ;
+  }
+});
+
+module.exports = Login;
