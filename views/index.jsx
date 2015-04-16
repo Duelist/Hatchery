1c9c09e3ea26101e35404b4a13cb938c9f681bc9
Ian Benedict <ianbenedict@gmail.com>
2015-04-16 12:28:55 -0400

    Added campaign list to index template.

--- views/index.jsx
+++ views/index.jsx
@@ -6,11 +6,32 @@ var Index = React.createClass({
   render: function() {
     if (this.props.member) {
       return (
-        <div>Welcome, {this.props.member.username}.</div>
+        <div>
+          <div>Welcome, {this.props.member.username}.</div>
+          <CampaignList></CampaignList>
+        </div>
       );
     }
 
-    return <a href='/login'>Log In</a>;
+    return (
+      <div>
+        <div><a href='/login'>Log In</a></div>
+        <div><a href='/login'>Sign Up</a></div>
+      </div>
+    );
+  }
+});
+
+var CampaignList = React.createClass({
+  render: function () {
+    return (
+      <ul>
+        {this.props.member.campaigns.map(function (campaign) {
+          var campaign_link = '/campaigns/' + campaign.slug;
+          return <li><a href={campaign_link}>{campaign.name}</a></li>
+        })}
+      </ul>
+    );
   }
 });
 
