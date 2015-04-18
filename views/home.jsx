551cf12c080b800e62718ce28aa1f4746c35d509
Ian Benedict <ianbenedict@gmail.com>
2015-04-17 20:55:38 -0400

    Added more inline styling for testing.

--- views/home.jsx
+++ views/home.jsx
@@ -5,12 +5,35 @@ var React = require('react');
 var Home = React.createClass({
   render: function() {
     return (
-      <html>
+      <html style={{height: '100%'}}>
         <head></head>
-        <body style={{backgroundColor: '#A5DE37', color: '#FFFFFF'}}>
-          <div>
-            <div><a href='/login'>Log In</a></div>
-            <div><a href='/member/new'>Sign Up</a></div>
+        <body style={{
+          backgroundColor: '#A5DE37',
+          fontFamily: 'Helvetica Neue',
+          color: '#FFFFFF',
+          width: '100%',
+          height: '100%'
+        }}>
+          <div style={{display: 'table'}}>
+            <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+              <div style={{fontSize: '48px', paddingBottom: '40px'}}>Your new campaign manager.</div>
+              <div>
+                <a href='/member/new' style={{
+                  fontSize: '30px',
+                  width: '150px',
+                  height: '60px',
+                  display: 'table',
+                  border: '2px solid #ffffff',
+                  borderRadius: '8px',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }}>
+                  <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
+                    Sign Up
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
         </body>
       </html>
